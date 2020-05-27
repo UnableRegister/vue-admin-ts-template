4a1c64cb4c3811927d67b3c920a0aa8b96639854
IS404 <unableregister@gmail.com>
2020-05-27 15:02:13 +0800

    init

---
+++ src/views/table/d2-crud.vue
@@ -0,0 +1,46 @@
+/*
+ * @Author: Wind Rises
+ * @Date: 2020-05-27 09:26:34
+ * @Last Modified by: Wind Rises
+ * @Last Modified time: 2020-05-27 11:11:55
+ */
+<template>
+  <d2-container>
+    <d2-crud ref="d2Crud" :columns="columns" :data="data" :options="options" />
+  </d2-container>
+</template>
+
+<script lang='ts'>
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class extends Vue {
+  columns: any[] = [
+    {
+      title: '名字',
+      key: 'name',
+      width: '140'
+    },
+    {
+      title: '名字',
+      key: 'age',
+      width: '140'
+    }
+  ]
+
+  data: any[] = [
+    {
+      name: 'ALI',
+      age: 18
+    },
+    {
+      name: 'Hsak',
+      age: 48
+    }
+  ]
+
+  options = {
+    border: true
+  }
+}
+</script>
